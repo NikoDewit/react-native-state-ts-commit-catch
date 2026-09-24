773b725ef6384d624606aa4a7e653003082fd809
NikoDewit <niko.dewit1@gmail.com>
2026-09-24 09:37:29 -0600

    add setTodo to addTodoItem

--- components/TodoSection.jsx
+++ components/TodoSection.jsx
@@ -17,6 +17,7 @@ export function TodoSection() {
   const addTodoItem = (title) => {
     const newTodo = { id: generateId(), title };
     // add item to the existing array
+    setTodos((prev) => [...prev, newTodo]);
   };
 
   const deleteTodoItem = (id) => {
