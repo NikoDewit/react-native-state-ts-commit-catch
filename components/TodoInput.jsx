b4d1d894d5d2ad867413aeb40a4044f19f015210
NikoDewit <niko.dewit1@gmail.com>
2026-09-24 08:53:19 -0600

    delete comment

--- components/TodoInput.jsx
+++ components/TodoInput.jsx
@@ -4,7 +4,6 @@ import { AppButton } from "./AppButton";
 import { colors } from "../assets/theme";
 
 export function TodoInput({ onAddTodo }) {
-  // TODO (state): replace this with a useState hook so the TextInput is
   const [enteredTodo, setEnteredTodo] = useState("placeholder Text");
 
   // TODO (state): update enteredTodo with the text the user typed.
